f3def15bd1f94a89673a63a14e996a56b74a390d
Imhotep <imhotep.php@gmail.com>
2024-11-12 13:47:37 +0600

    Add default route 404

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use App\Http\Controllers\WelcomeController;
 use Imhotep\Facades\Route;
 
+// Call the default action if the route is missing
+Route::setDefaultAction(fn() => abort(404) );
+
 Route::get('/', WelcomeController::class);
